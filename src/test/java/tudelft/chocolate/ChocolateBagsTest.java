fd64d33bd12480108c0a049a25acd346f3d9d5c8
Michel van Heijningen <vanheijningen.michel@gmail.com>
2018-11-27 21:51:30 +0100

    GHappy test class

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -1,10 +1,12 @@
 package tudelft.chocolate;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class ChocolateBagsTest {
+    @DisplayName("Total is too big")
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
     public void totalIsTooBig(int small, int big, int total, int expectedResult) {
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,40 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    GHappy gHappy;
+
+    @BeforeEach
+    public void initialize() {
+        this.gHappy = new GHappy();
+    }
+
+    @Test
+    public void oneG() {
+        Assertions.assertFalse(gHappy.gHappy("g"));
+    }
+
+    @Test
+    public void oneNonG() {
+        Assertions.assertTrue(gHappy.gHappy("h"));
+    }
+
+    @Test
+    public void happyG() {
+        Assertions.assertTrue(gHappy.gHappy("buggy"));
+    }
+
+    @Test
+    public void unhappyG() {
+        Assertions.assertFalse(gHappy.gHappy("google"));
+    }
+
+    @Test
+    public void multipleNonG() {
+        Assertions.assertTrue(gHappy.gHappy("happy"));
+    }
 }
