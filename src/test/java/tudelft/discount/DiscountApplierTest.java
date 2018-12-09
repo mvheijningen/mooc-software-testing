c4a4a7521ff54fa49e57040623d3c9fa1c5611ff
Michel van Heijningen <vanheijningen.michel@gmail.com>
2018-12-09 21:54:23 +0100

    debug DiscountApplier

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,25 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DiscountApplierTest {
+
+    @Test
+    public void applyDiscount() {
+        Product homeProduct = new Product("homeProduct", 10, "HOME");
+        Product businessProduct = new Product("businessProduct", 10, "BUSINESS");
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        List<Product> allProducts = Arrays.asList(homeProduct, businessProduct);
+        Mockito.when(dao.all()).thenReturn(allProducts);
+        DiscountApplier discounter = new DiscountApplier(dao);
+        discounter.setNewPrices();
+        Assertions.assertEquals(9, allProducts.get(0).getPrice());
+        Assertions.assertEquals(11, allProducts.get(1).getPrice());
+    }
 
 }
